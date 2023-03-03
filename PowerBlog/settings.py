edf49c3f11b03d3f6db3fe743bb899ecd4f10fed
adey-px <aosteph@gmail.com>
2023-03-03 11:53:26 -0600

    App deployed to AWS Elastic Beanstalk

--- PowerBlog/settings.py
+++ PowerBlog/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://powerblog-env.eba-2kpd8mtr.us-east-1.elasticbeanstalk.com/']
 
 
 # Application definition
