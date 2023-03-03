5365732eec0755cd8daa3e59147f1f3fc95b4e3f
adey-px <aosteph@gmail.com>
2023-03-03 17:43:42 -0600

    Installed django-ebhealthcheck

--- PowerBlog/settings.py
+++ PowerBlog/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "ebhealthcheck.apps.EBHealthCheckConfig",
     "article",
     "crispy_forms",
     "crispy_bootstrap5",
