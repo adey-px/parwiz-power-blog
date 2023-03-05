9bc08141ed8c4c62d74f9b2dfc4b8249ae8f826a
adey-px <aosteph@gmail.com>
2023-03-05 17:11:41 -0600

    Updates

--- PowerBlog/settings.py
+++ PowerBlog/settings.py
@@ -19,11 +19,7 @@ SECRET_KEY = "django-insecure-+*=bcsy&=&^2uftiw6566lw9n!ncsub$pex"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.0",
-    "powerblog-env.eba-gwm5e5d6.us-east-1.elasticbeanstalk.com"
-]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
